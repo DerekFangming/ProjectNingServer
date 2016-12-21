3336afd6f4e63cb69978ab0d0e85acc26ed4d470
DerekFangming <synfm@126.com>
2016-12-20 22:57:33 -0500

    Use Scalr library for image resizing

--- src/main/java/com/projectning/service/dao/impl/jdbc/JdbcBaseDao.java
+++ src/main/java/com/projectning/service/dao/impl/jdbc/JdbcBaseDao.java
@@ -15,7 +15,6 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.RowMapperResultSetExtractor;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
--- src/main/java/com/projectning/service/web/MomentController.java
+++ src/main/java/com/projectning/service/web/MomentController.java
@@ -1,10 +1,20 @@
 package com.projectning.service.web;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.imageio.ImageIO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,6 +29,7 @@ import com.projectning.service.manager.ImageManager;
 import com.projectning.service.manager.MomentManager;
 import com.projectning.service.manager.UserManager;
 import com.projectning.util.ImageType;
+import com.projectning.util.Scalr;
 import com.projectning.util.Util;
 
 @Controller
@@ -71,7 +82,45 @@ public class MomentController {
 			respond = Util.createErrorRespondFromException(e);
 		}
 		return new ResponseEntity<Map<String, Object>>(respond, HttpStatus.OK);
-		
 	}
+	
+	@RequestMapping("/get_moment_cover_image")
+    public ResponseEntity<Map<String, Object>> getMomentCoverImage(@RequestBody Map<String, Object> request) {
+		Map<String, Object> respond = new HashMap<String, Object>();
+		//BufferedImage newImage = new BufferedImage(60, 60, BufferedImage.TYPE_INT_ARGB);
+
+	    //Graphics2D g = (Graphics2D) newImage.getGraphics();
+	    
+	    File img = new File("/Volumes/Data/images/15.jpg");
+		BufferedImage buffImg;
+		try {
+			buffImg = ImageIO.read(img);
+			buffImg = Scalr.resize(buffImg,  Scalr.Method.SPEED, Scalr.Mode.FIT_TO_WIDTH,
+                    60, 60, Scalr.OP_ANTIALIAS);
+			final ByteArrayOutputStream os = new ByteArrayOutputStream();
+		    try {
+		        ImageIO.write(buffImg, "jpg", Base64.getEncoder().wrap(os));
+		        respond.put("img", os.toString(StandardCharsets.ISO_8859_1.name()));
+		    } catch (final IOException ioe) {
+		        //
+		    }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+		return new ResponseEntity<Map<String, Object>>(respond, HttpStatus.OK);
+	}
+	
+	private BufferedImage resize(BufferedImage img, int newW, int newH) { 
+	    Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_DEFAULT);
+	    BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+	    Graphics2D g2d = dimg.createGraphics();
+	    g2d.drawImage(tmp, 0, 0, null);
+	    g2d.dispose();
+
+	    return dimg;
+	} 
 
 }
